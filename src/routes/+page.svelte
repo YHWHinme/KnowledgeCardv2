a3f942c545ec923834c90bf525f2b2cd730669fd
Archinme <brokenmopflag@gmail.com>
2025-10-20 23:57:30 +0100

    Added Knowledge Card Components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { invoke } from "@tauri-apps/api/core";
 
-	import * as lib from "../lib/lib"
+	import * as lib from "../lib/lib";
+
+	// Importing components
+	import KnowledgeCard from "$lib/components/KnowledgeCard.svelte";
 </script>
 
-<main class="bg-black text-teal-400">
-	<div>Hello</div>
+<main class="bg-background text-accent">
+	<KnowledgeCard />
 </main>
