085651a5bd2d2e997e11c0d42de821d80352f6d0
UNIinme <brokenmopflag@gmail.com>
2025-10-21 12:35:58 +0100

    Setup dynamic routing
    Added a few todos + Sidebar Rendering

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import "../app.css";
+	import Sidebar from "../lib/components/Sidebar.svelte"
+
+	const projects = [
+		{id: 1, title: "Project 1"},
+		{id: 2, title: "Project 2"},
+		{id: 3, title: "Project 3"},
+	]
 
 	let { children } = $props();
 </script>
 
 <main>
+	<!-- TODO: Setup css grid -->
+	<Sidebar {projects} />
+
 	{@render children()}
 </main>
