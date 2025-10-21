505afc32ed008a3e994b6343cc51ffcf54c3722a
UNIinme <brokenmopflag@gmail.com>
2025-10-21 12:35:58 +0100

    Setup dynamic routing
    Added a few todos + Sidebar Rendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
-	import { invoke } from "@tauri-apps/api/core";
-
-	import * as lib from "../lib/lib";
-
 	// Importing components
-	import KnowledgeCard from "$lib/components/KnowledgeCard.svelte";
 </script>
 
 <main class="bg-background text-accent">
-	<KnowledgeCard />
+	Hello
 </main>
