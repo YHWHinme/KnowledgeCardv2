afa370eb5b1bf8292ba5f6e58e70463f68fd26b6
UNIinme <brokenmopflag@gmail.com>
2025-10-21 14:38:20 +0100

    Setup colours and Css grid
    Added Todo for colours

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -2,7 +2,7 @@
 	export let projects: {id: number, title: string}[];
 </script>
 
-<div class="w-64 bg-base-200 h-screen p-4">
+<div class="bg-amber-50 bg-base-200 h-screen p-4">
 	<h2 class="text-xl font-bold mb-4">Projects</h2>
 	<ul class="menu bg-base-100 rounded-box">
 		{#each projects as project}
