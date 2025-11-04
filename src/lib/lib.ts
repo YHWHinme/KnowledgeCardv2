614cf74ae14ced89bfe729e723732de40f47e50b
UNIinme <brokenmopflag@gmail.com>
2025-11-04 15:05:59 +0000

    Backend Logic update
    Got doppler working and added rename and select logic

--- src/lib/lib.ts
+++ src/lib/lib.ts
@@ -1,4 +1,3 @@
-import Databse from "@tauri-apps/plugin-sql";
 import { createClient } from "@libsql/client";
 
 export const db = createClient({
@@ -15,26 +14,86 @@ interface Projects {
 export async function getAllProjects(): Promise<Projects[] | number> {
   try {
     const result = await db.execute("SELECT * FROM projects");
-    return result.rows as Projects;
+    return (result.rows ?? []) as unknown as Projects[];
   } catch (err) {
     console.log(err);
     return 500;
   }
 }
 
+export async function renameProject(
+  projectId: number,
+  new_title: string,
+): Promise<number> {
+  try {
+    await db.execute("UPDATE projects SET title=? WHERE id=?", [
+      new_title,
+      projectId,
+    ]);
+    return 200;
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
+}
+
+export async function deleteProject(projectId: number): Promise<number> {
+  try {
+    // TODO: Ensure there's an alert confirming the deletion
+    // I need to delete all tasks associated with this project too
+    await db.execute("DELETE FROM projects WHERE id=?", [projectId]);
+    return 200;
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
+}
+
+// blob based functions
+interface Blobs {
+  id: number;
+  blobName: string;
+  description: string;
+  isFunction: Boolean;
+  blobValue: string; // TODO: May not be a string, check
+  projectId: number;
+}
+
+export async function getALlBlobs(): Promise<Blobs[] | number> {
+  try {
+    const result = await db.execute("SELECT * FROM blobitems");
+    return (result.rows ?? []) as unknown as Blobs[];
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
+}
+
+export async function renameBlob(
+  blobId: number,
+  blobName: string,
+): Promise<number> {
+  try {
+    await db.execute("UPDATE blobItems SET blobName=? WHERE id=?", [
+      blobName,
+      blobId,
+    ]);
+    return 200;
+  } catch (err) {
+    console.log(err);
+    return 500;
+  }
+}
 // Schema
 //
-// CREATE TABLE blobItem(
-//     id INTEGER PRIMARY KEY AUTOINCREMENT,
-//     itemName TEXT NOT NULL,
+// CREATE TABLE blobItem( //     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//     blobName TEXT NOT NULL,
 //     description TEXT,
 //     isFunction BOOLEAN NOT NULL,
 //     blobValue TEXT NOT NULL,
-//     parentId INTEGER,
-//     CONSTRAINT fk_parent_id FOREIGN KEY (parentId) REFERENCES projects(id);
+//     projectId INTEGER,
+//     CONSTRAINT fk_project_id FOREIGN KEY (projectId) REFERENCES projects(id)
 // );
-//
-// CREATE TABLE projects(
 //     id INTEGER PRIMARY KEY AUTOINCREMENT,
 //     title TEXT NOT NULL
 // );
